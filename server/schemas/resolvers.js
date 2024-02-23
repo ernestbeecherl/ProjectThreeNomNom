007daac80fbebc5724003ec424f975bce9cb1250
Anthony Guerra <tx.tony9@gmail.com>
2024-02-22 21:26:25 -0800

    added to typedefs and resolvers and deleted api key and put it into a dotenv file

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -32,6 +32,32 @@ const resolvers = {
       }
     },
   },
+
+Mutation: {
+    addUser: async (parent, args) => {
+      const user = await User.create(args);
+      const token = signToken(user);
+
+      return { token, user };
+    },
+    login: async (parent, { email, password }) => {
+        const user = await User.findOne({ email });
+  
+        if (!user) {
+          throw AuthenticationError;
+        }
+  
+        const correctPw = await user.isCorrectPassword(password);
+  
+        if (!correctPw) {
+          throw AuthenticationError;
+        }
+  
+        const token = signToken(user);
+  
+        return { token, user };
+      }
+    }
 };
 
 module.exports = resolvers;
--- server/utils/api.js
+++ server/utils/api.js
@@ -3,6 +3,6 @@ require('dotenv').config();
 
 const yelp = new Yelp({
     clientId: process.env.API_KEY, //|| 'WxNvD3CiqGyoYHtVCP7UzA'
-    clientSecret: process.env.API_SECRET //|| 'bPO5F5aA4ZudgL235rrJeRWV8Y1jtL3TRdUrv0ByTh_lugiHQRkqvx-7Kibr0sRZyaM8HlEyEF1bX2zKMaX4HY_V5QS3i14QFsKvhOYUV5gQGesl-smewWgger7OZXYx'
+    clientSecret: process.env.API_SECRET //|| 'joyVR4sZF7Iw4usNaD0b4UM5KgNvrs67HFZ9SecXMjT5966uzr9ENXIndrmx5EMx8eQzOGp23x1sf8GG9JDB1qPCH_m3kws18wvnL0bodJ8a78JJ7ATc1xYfPvnXZXYx'
 
 });
