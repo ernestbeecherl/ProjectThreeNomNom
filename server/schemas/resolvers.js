97276bd0d061fd7c30febefe973d63d7fbc016ee
Anthony Guerra <tx.tony9@gmail.com>
2024-02-22 17:57:47 -0800

    added api key resolvers.js in the server folder

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -10,7 +10,7 @@ const resolvers = {
         // Make a request to Yelp API
         const response = await axios.get('https://api.yelp.com/v3/businesses/search', {
           headers: {
-            Authorization: `Bearer YOUR_YELP_API_KEY`,
+            Authorization: `Bearer joyVR4sZF7Iw4usNaD0b4UM5KgNvrs67HFZ9SecXMjT5966uzr9ENXIndrmx5EMx8eQzOGp23x1sf8GG9JDB1qPCH_m3kws18wvnL0bodJ8a78JJ7ATc1xYfPvnXZXYx`,
           },
           params: {
             term,
